1ea895e39f875d9c80e11f80b5cb3530e4408e8e
AFZAL  HUSSAIN <167523515+shaikafzalhussain@users.noreply.github.com>
2024-08-08 13:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "student.c5ylf3yirenb.us-east-2.rds.amazonaws.com"
-customuser = "aarmanvu"
-custompass = "thinkbig1995"
-customdb = "student"
-custombucket = "addstudent101"
-customregion = "us-east-2"
+customhost = "employee.cdiwoss0ye40.ap-south-1.rds.amazonaws.com"
+customuser = "rooman"
+custompass = "afzutori"
+customdb = "employee"
+custombucket = "employeename.project"
+customregion = "ap-south-1"
 # it is editable according to rds and s3 
