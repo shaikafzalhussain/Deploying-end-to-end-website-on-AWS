149439cba32136e413e76d4a0a91c067164095e8
AFZAL  HUSSAIN <167523515+shaikafzalhussain@users.noreply.github.com>
2024-08-08 13:12:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "student.c5ylf3yirenb.us-east-2.rds.amazonaws.com"
+customuser = "aarmanvu"
+custompass = "thinkbig1995"
+customdb = "student"
+custombucket = "addstudent101"
+customregion = "us-east-2"
+# it is editable according to rds and s3 
